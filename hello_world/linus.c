293d334a9baf9bb82e1a87fb5b0561768c1a8292
Daniel Alzugaray <dalzuga@gmail.com>
2016-01-30 23:01:16 +0000

    removed linus.c

--- hello_world/linus.c
+++
@@ -1,8 +0,0 @@
-//#include
-
-int main(void)
-{
-  puts("Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.");
-  puts("- Linus Torvalds");
-  return (98);
-}
