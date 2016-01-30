b6f7b8f9103d1483079a94e0077d52f123f4414d
Daniel Alzugaray <49@p3-5.hbtn.io>
2016-01-30 12:07:15 -0800

    first commit

---
+++ linus.c
@@ -0,0 +1,21 @@
+Welcome to Ubuntu 14.04.3 LTS (GNU/Linux 3.13.0-74-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com/
+
+  System information as of Sat Jan 30 19:43:18 UTC 2016
+
+  System load:  0.0               Processes:           75
+  Usage of /:   3.4% of 39.34GB   Users logged in:     0
+  Memory usage: 25%               IP address for eth0: 10.0.2.15
+  Swap usage:   0%
+
+  Graph this data and manage this system at:
+    https://landscape.canonical.com/
+
+  Get cloud support with Ubuntu Advantage Cloud Guest:
+    http://www.ubuntu.com/business/services/cloud
+
+0 packages can be updated.
+0 updates are security updates.
+
+
