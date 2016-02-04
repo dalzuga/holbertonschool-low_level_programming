396417e9c5278c150e06e12033a890a4aaf49c1b
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-04 17:46:52 +0000

    testing

--- i_m_not_in_the_loop/1-print_alphabet.c
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -1,5 +1,7 @@
 #include "my_functions.h"
 
+void print_alphabet(void);
+
 int main(void)
 {
   print_alphabet();
--- i_m_not_in_the_loop/my_functions.h
+++ i_m_not_in_the_loop/my_functions.h
@@ -1,9 +1,5 @@
 #include <unistd.h>
 
-int print_char(char c);
-void print_z(void);
-void print_alphabet(void);
-
 int print_char(char c)
 {
   return (write(1, &c, 1));
@@ -15,14 +11,13 @@ void print_z(void)
   print_char('\n');
 }
 
-/*
 void print_alphabet(void)
-int i;
-
 {
+  int i;
+
   for (i=0;i<26;i++)
     {
-      print_char((char (i+97)));
+      print_char(((char) i)+97);
     }
-  print_char("\n");
-} */
+  print_char('\n');
+}
