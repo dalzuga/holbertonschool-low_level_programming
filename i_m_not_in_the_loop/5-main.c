14ee35a81f6121ae6285d4726fbe2a3de0dca166
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-05 16:31:28 +0000

    update

--- i_m_not_in_the_loop/5-main.c
+++ i_m_not_in_the_loop/5-main.c
@@ -2,5 +2,26 @@
 
 int main(void)
 {
-  /*  your function call here */
+  print_number(98);
+  print_char('\n');
+  print_number(402);
+  print_char('\n');
+  print_number(1024);
+  print_char('\n');
+  print_number(0);
+  print_char('\n');
+  print_number(-98);
+  print_char('\n');
+
+  print_number(1);
+  print_char('\n');
+  print_number(10);
+  print_char('\n');
+  print_number(11);
+  print_char('\n');
+  print_number(100);
+  print_char('\n');
+  print_number(101);
+  print_char('\n');
+  return (0);
 }
