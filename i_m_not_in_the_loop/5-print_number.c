f5a8f89cd612205c63f5ddf3a8694bf529dd3efa
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-08 22:00:36 +0000

    update

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -6,15 +6,19 @@ void print_number(int n)
   int j=0;
   int x=10;
 
-  char a='0',b; // b not applicable for this task
+  char a='0',b='0'; /* not applicable for this task */
   
+  print_char(b);
+  print_char(j+49);
+
+
   while (x<n && i<10)
     {
       x=x*10;
       i++;
     }
 
-  if (x<n) // handles special case where number is 10 digits long
+  if (x<n) /* handles special case where number is 10 digits long */
     {
       x=2147483647;
       i++;
@@ -23,14 +27,13 @@ void print_number(int n)
   if (i<10)
     {
       b = (char) (i+48);
-      j = 0;
-      print_char(b);
+      j=0;
     }
-  else // not applicable for this task, but allows for integers that are longer than 32-bit
+  else /* not applicable for this task, but allows for integers that are longer than 32-bit */
     {
       a=49;
       b=i;
-      print_char((char) 49);
+      print_char(a);
       print_char((char) (i+38));
       j=1;
     }
@@ -39,6 +42,6 @@ void print_number(int n)
 
   for (;i>1;i--)
     {
-      print_char(b);
+      print_char((char) b);
     }
 }
