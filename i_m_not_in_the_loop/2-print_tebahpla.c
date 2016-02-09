761651daa201bd313dbf1a569474f19a81ed7506
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-08 23:12:15 -0800

    only comments now for the last task

--- i_m_not_in_the_loop/2-print_tebahpla.c
+++ i_m_not_in_the_loop/2-print_tebahpla.c
@@ -4,8 +4,8 @@ void print_tebahpla(void)
 {
   int i;
 
-  for (i=26;i>0;i--) // start at z
+  for (i=26;i>0;i--) /* start at z */
     {
-      print_char(((char) i)+96); // add 97 to match ASCII code for the integer
+      print_char(((char) i)+96); /* add 97 to match ASCII code for the integer */
     }
 }
--- i_m_not_in_the_loop/4-positive_or_not.c
+++ i_m_not_in_the_loop/4-positive_or_not.c
@@ -2,10 +2,10 @@
 
 void positive_or_not(int n)
 {
-  if (n>0) // if greater than 0
-    print_char('P'); // print P for positive
-  else if (n==0) // if it is 0
-    print_char('Z'); // print Z for zero
-  else // otherwise, the number must be negative
-    print_char('N'); // print N for negative
+  if (n>0) /* if greater than 0 */
+    print_char('P'); /* print P for positive */
+  else if (n==0) /* if it is 0 */
+    print_char('Z'); /* print Z for zero */
+  else /* otherwise, the number must be negative */
+    print_char('N'); /* print N for negative */
 }
