ed9239774f12cb93f84def5e162f86808b9376ad
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-09 02:10:09 +0000

    done except for the special case; thinking strategy

--- i_m_not_in_the_loop/5-print_number.c
+++ i_m_not_in_the_loop/5-print_number.c
@@ -2,12 +2,15 @@
 
 void print_number(int n)
 {
-  if (n<0) print_char('-'); if (n<0) n=-n;
+  if (n<0)
+    print_char('-');
+  if (n<0)
+    n=-n;
   int i,count=count_digits(n),x=1;
   for (i=1;i<count;i++)
     x=x*10;
   print_char((n/x)+48);
   
   if (x>1)
-  print_number(n - ((int) n/x) * x);
+    print_number(n - ((int) n/x) * x);
 }
