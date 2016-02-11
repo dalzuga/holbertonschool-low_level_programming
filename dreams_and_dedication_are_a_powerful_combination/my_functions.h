d530c656f75abf03b894ff22864378ee7d301bb7
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-11 21:12:29 +0000

    asdf

--- dreams_and_dedication_are_a_powerful_combination/my_functions.h
+++ dreams_and_dedication_are_a_powerful_combination/my_functions.h
@@ -1,8 +1,4 @@
 int print_char(char c); /* original function to print one single character */
-void print_z(void); /* declaration of print_z function */
-void print_alphabet(void); /* declaration of print_alphabet function */
-void print_tebahpla(void); /* etc */
-void print_base16(void); /* etc */
-void positive_or_not(int n); /* etc */
 void print_number(int n); /* etc */
 int count_digits(int n); /* this is my function that counts the digits */
+void print_combination_1(void);
