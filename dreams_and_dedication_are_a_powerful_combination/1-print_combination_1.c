2b20d5030f81163da1abc5204c14eca4b37c5015
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-11 21:22:19 +0000

    commented task 1

--- dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -4,12 +4,12 @@ void print_combination_1(void)
 {
   int i;
 
-  print_char(48);
+  print_char(48); /* prints 0 */
 
-  for (i=1;i<10;i++)
+  for (i=1;i<10;i++) /* runs 9 times */
     {
-      print_char(',');
-      print_char(' ');
-      print_char(i+48);
+      print_char(','); /* print comma */
+      print_char(' '); /* print space */
+      print_char(i+48); 	/* print corresponding number */
     }
 }
