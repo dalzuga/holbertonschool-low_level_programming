626be5973a3ad91e2f79a06c07891028f2bd66bd
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-12 00:10:53 +0000

    task 4 commented

--- dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
+++ dreams_and_dedication_are_a_powerful_combination/4-print_combination_4.c
@@ -9,15 +9,15 @@ void print_combination_4(void)
       for (j=i+1;j<100;j++) 	/* runs all the way up to 99 */
 	{
 	  if (i<10)
-	    print_char('0');
+	    print_char(48);	/* add leading 0 for i<10 */
 
-	  print_number(i);
+	  print_number(i);	/* print first number */
 	  print_char(' ');	/* print space */
 
 	  if (j<10)
-	    print_char('0');
+	    print_char(48);	/* add leading 0 for j<10 */
 
-	  print_number(j);
+	  print_number(j);	/* print second number */
 
 	  if (!(i==98 && j==99)) 	/* make sure it's not the last number */
 	    {
