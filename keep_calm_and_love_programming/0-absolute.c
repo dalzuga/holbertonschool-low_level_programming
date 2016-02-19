1a3fc35065a15714719124e1af9d1bb28c43dfe7
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-19 00:56:09 +0000

    tasks 0 and 1 files tested

--- keep_calm_and_love_programming/0-absolute.c
+++ keep_calm_and_love_programming/0-absolute.c
@@ -1,7 +1,7 @@
 int absolute(int n)
 {
   if (n<0)
-    return -n; // print positive if negative
+    return -n;  /* print positive if negative */
 
-  return n; // return same number if positive
+  return n; /* return same number if positive */
 }
