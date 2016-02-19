0e17f899497c27047315ebbeac2a640b75776615
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-19 00:43:52 +0000

    comments

--- keep_calm_and_love_programming/1-uppercase.c
+++ keep_calm_and_love_programming/1-uppercase.c
@@ -1,7 +1,7 @@
 char uppercase(char c)
 {
-  if (c>96 && c<123)
-    return c-32;
-  return c;
+  if (c>96 && c<123) // if lowercase letter
+    return c-32; // return uppercase
+  return c; // if not lowercase letter, return unchanged
 }
 
