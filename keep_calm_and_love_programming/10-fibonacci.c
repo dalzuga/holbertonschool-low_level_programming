13fbbc60a94b1190bd6411f1e3ec008321b49d06
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-19 04:57:21 +0000

    task 10 done

---
+++ keep_calm_and_love_programming/10-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  long int a, b, c, sum;
+  
+  a=1, b=2;		/* first two fibonacci numbers */
+  sum=2;		/* sum of even ones so far */
+
+  while (a+b < 4000000)	/* while the next Fibonacci is less than 4x10^6 */
+    {
+      c=a+b; 			/* go to next Fibonacci number */
+      if (c%2==0)		/* if even, add to sum */
+	sum+=c;
+      a=b; b=c;
+    }
+
+  printf("%ld", sum);
+
+  return 0;
+}
