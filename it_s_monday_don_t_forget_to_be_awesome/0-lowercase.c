3d11952740d2fd6b2474fb01d6f7f049ef24cf5b
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-23 18:09:53 +0000

    commented

--- it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
+++ it_s_monday_don_t_forget_to_be_awesome/0-lowercase.c
@@ -1,6 +1,6 @@
 char lowercase(char c)
 {
-  if (c >=65 && c <= 90)
-    return c+32;
-  return c;
+  if (c >=65 && c <= 90) 	/* if uppercase letter */
+    return c+32;		/* return lowercase */
+  return c;			/* else, return without change */
 }
