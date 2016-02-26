4d60eca73adbbb8f0d7f20ffe9ba9d5ce8fa7ee3
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-26 18:16:36 +0000

    task 0 done

---
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void triangles_in_the_term(int h, int n)
+{
+  int i, j, k;
+
+  for (k = 0; k < n; k++)
+    {
+      for (j = 0; j < h; j++) 	/* j is the line number starting at line 0 */
+	{
+	  for (i = h-1-j; i > 0; i--) 	/* start with the spaces */
+	    {
+	      printf(" ");
+	    }
+  
+	  for (i = 1; i <= 2*j+1; i++)  	/* draw asterisks */
+	    {
+	      printf("*");
+	    }
+	  printf("\n");
+	}
+    }
+}
