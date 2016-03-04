55581fe3bc50a095bcf825e4211afb71c4d0da30
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-04 23:06:36 +0000

    task 3 done!

--- pointers_are_fun/3-print_string.c
+++ pointers_are_fun/3-print_string.c
@@ -1,4 +1,9 @@
+int print_char(char);
 void print_string(char *str)
 {
-  
+  while (*str != '\0') 		/* while string isn't over */
+    {
+      print_char(*str); 	/* print value pointed to by str */
+      str++;			/* pointer arithmetic for next char */
+    }
 }
