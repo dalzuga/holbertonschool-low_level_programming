f62324bf906afda9bfd30e1c7666b70152294774
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-05 02:24:53 +0000

    removed dangerous stuff

--- pointers_are_fun/4-reverse_string.c
+++ pointers_are_fun/4-reverse_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 int str_len(char *str);
 
 void reverse_string(char *s)
@@ -8,26 +7,11 @@ void reverse_string(char *s)
 
   length = str_len(s); 		/* obtain string length */
 
-  printf("%d\n", length);
-  printf("length/2: %d\n", length/2);
-
-  for (i = 0; i < length; i++)
-    {
-      printf("s[%d] = %c\n", i, *(s + i));
-    }
-
   for (i = 0; i < length/2; i++) /* split the string in 2 */
     {
       c = *(s + i);		    /* c holds char in left half */
       *(s + i) = *(s + length - i - 1); /* left half becomes right half  */
       *(s + length - i - 1) = c;	/* right half becomes old left half */
-      printf("*(s+i) = %c ||| ", *(s+i));
-      printf("*(s+length-i) = %c\n", *(s+length-i));
-    }
-
-  for (i = 0; i < length; i++)
-    {
-      printf("s[%d] = %c\n", i, *(s + i));
     }
 }
 
