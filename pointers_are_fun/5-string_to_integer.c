25417c3e32a513545a486992e24607cff118a4a4
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-07 08:04:36 +0000

    fixed up

--- pointers_are_fun/5-string_to_integer.c
+++ pointers_are_fun/5-string_to_integer.c
@@ -1,20 +1,22 @@
-char negative(char *s, char *sign)
+#include <stdio.h>
+
 int string_to_integer(char *s)
 {
   char sign;			/* keep track of negative signs */
   int n;			/* integer to return, unsigned */
 
   sign = 0;
-  number = 0;
+  n = 0;
 
-  while (s != '\0')
+  while (*s != '\0')
     {
-      if (s == '-')
-	sign = (sign + 1) % 2; 	/* 0 means positive, 1 means negative */
-      
-      if (s >= '0' && s <= '9')	/* if character is in [0-9] */
+      if (*s == '-')
 	{
-	  n = n * 10 + (int)(s - '0'); /* make it part of the int */
+	  sign = (sign + 1) % 2; 	/* 0 means positive, 1 means negative */
+	}
+      if (*s >= '0' && *s <= '9')	/* if character is in [0-9] */
+	{
+	  n = n * 10 + (int)(*s - '0'); /* make it part of the int */
 	}
       s++;
     }
@@ -22,10 +24,4 @@ int string_to_integer(char *s)
   if (sign)
     return -n;
   return n;
-}
-
-char negative(char *s, char *sign)
-{
-  if (s == '-')
-    sign = (sign + 1) % 2;
 }
