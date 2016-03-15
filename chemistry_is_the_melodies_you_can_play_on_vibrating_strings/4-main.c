e917197bb4fb2f070c0d6790ecf4c0ad16f8a48c
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-15 08:20:19 +0000

    update

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-main.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 int strings_compare(char *s1, const char *s2);
 
 int main(void)
 {
   char s1[] = "Hello";
-  char s2[] = "World!";
+  char s2[] = "Horld!";
 
   printf("%d\n", strings_compare(s1, s2));
+  printf("strcmp: %d\n", strcmp(s1, s2));
   printf("%d\n", strings_compare(s2, s1));
+  printf("strcmp: %d\n", strcmp(s2, s1));
   printf("%d\n", strings_compare(s1, s1));
+  printf("strcmp: %d\n", strcmp(s1, s1));
   return (0);
 }
--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/4-strings_compare.c
@@ -1,16 +1,12 @@
 int strings_compare(char *s1, const char *s2)
 {
-  char flag = 0;
-
   int i;
 
   for (i = 0; s1[i] != '\0'; i++) /* go through the string */
     {
       if (s1[i] != s2[i])		   /* compare char by char */
-	flag = 1;
+	return s1[i] - s2[i];		   /* if different, return diff */
     }
 
-  if (flag)			/* if different */
-    return *s1 - *s2; 		/* return the diff of first pair of char */
-  return 0;			/* else the strings are the same */
+  return 0;			/* strings are the same */
 }
