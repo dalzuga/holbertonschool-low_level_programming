d0ae42365e00c98c77d941e83e9eafbe9648d58c
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-16 02:40:40 +0000

    task 5 done

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/5-reverse_array.c
@@ -2,5 +2,12 @@ void print_array(int *a, int n);
 
 void reverse_array(int *a, int n)
 {
-  print_array(a, n);
+  int i, basket; 		/* counter and int holder */
+  
+  for (i = 0; i < n/2; i++) /* split array in 2 */
+    {
+      basket = a[i]; 		/* hold left integer */
+      a[i] = a[n - i - 1]; 	/* copy right to left */
+      a[n - i - 1] = basket;	/* put integer back, into right */
+    }
 }
