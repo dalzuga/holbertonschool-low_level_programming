dc83bc36f7ca012efd0cd16a99033eb340ecd5b2
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-24 23:41:01 +0000

    done

--- let_s_go_deeper/0-factorial.c
+++ let_s_go_deeper/0-factorial.c
@@ -1,4 +1,8 @@
 int factorial(int n)
 {
-  
+  if (n < 0)
+    return -1;
+  else if (n < 2)			/* if 0 or 1 */
+    return 1; 			/* return 1 */
+  return n * factorial(n - 1); /* recursively call function */
 }
