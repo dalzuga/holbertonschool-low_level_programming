b88c768247c1834a69f1c2aac4b58342f447b00a
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-24 23:53:41 +0000

    added comment

--- let_s_go_deeper/1-power.c
+++ let_s_go_deeper/1-power.c
@@ -2,8 +2,8 @@ int power(int x, int y)
 {
   if (y < 0)			/* if y lower than 0 */
     return -1;			/* return -1 */
-  else if (y == 0)
-    return 1;
+  else if (y == 0) 		/* if y equals 0 */
+    return 1;			/* return 1 */
   else if (y == 1)
     return x;			/* if no more, return x */
   return x * power(x, y - 1); /* recursively call function */
