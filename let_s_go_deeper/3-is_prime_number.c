68e36f3db46ced9d215cc7013e5bacef15ac9403
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-25 19:13:36 +0000

    task 3 done

--- let_s_go_deeper/3-is_prime_number.c
+++ let_s_go_deeper/3-is_prime_number.c
@@ -1,16 +1,16 @@
 int helper(int n, int a)
 {
-  if (n % a == 0)		/* if success */
-    return 0;			/* return value of a */
-  if (a == 1)			/* if a is 1, then non-natural sqrt */
-    return 1;			/* return -1 */
+  if (a == 1)			/* if a is 1, then prime */
+    return 1;			/* return 1 */
+  if (n % a == 0)		/* if not prime */
+    return 0;			/* return 0 */
   return helper(n, a - 1);	/* do recursive call */
 }
 
 int is_prime_number(int n)
 {
   if (n < 2)
-    return 0;			/* if 1, return 1 */
+    return 0;			/* if less than 2, not prime */
   
   return helper(n, n - 1); 	/* try a value less than n */
 }
