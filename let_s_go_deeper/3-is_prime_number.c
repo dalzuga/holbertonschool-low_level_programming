23b80ffa3e6287e1cda766f279951ab66e962029
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-25 00:41:13 +0000

    init task 3

---
+++ let_s_go_deeper/3-is_prime_number.c
@@ -0,0 +1,12 @@
+int is_prime_number(int n)
+{
+  int i;
+
+  for (i = 0; i <= n; i++)	/* try all numbers <= n */
+  {
+    if (i * i == n)		/* if the square root */
+      return i;			/* return the value */
+  }
+
+  return -1;			/* all values tried, no natural sqrt's */
+}
