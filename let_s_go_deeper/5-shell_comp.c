44627a5fb6bb7044a6feb42210c53734d13cca92
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-26 20:48:39 +0000

    saving progress - testing new idea

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -1,3 +1,11 @@
+int string_length(char * str)
+{
+  if (*str == '\0')
+    return 0;
+
+  return 1 + string_length(str + 1);
+}
+
 int shell_comp(char *s1, char *s2)
 {
   if (*s1 == '\0' && *s2 == '\0') /* if both strings are empty */
@@ -15,6 +23,13 @@ int shell_comp(char *s1, char *s2)
   /* s1 = "ab", s2 = "a*", remove first character of each */
   if (*s1 == *s2)		/* if the first characters of each string are equal */
     return shell_comp(s1 + 1, s2 + 1); /* move forward 1 character */
+
+  /* if the last characters of each string are equal, remove them */
+  if ( *(s1 + string_length(s1) - 1) == *(s2 + string_length(s2) - 1) )
+  {
+    *(s1 + string_length(s1) - 1) = '\0';
+    *(s2 + string_length(s2) - 1) = '\0';
+  }
 
   /* "xyza" and "*a"; keep going until s1 becomes "a" */
   if (*s2 == '*' && *(s2 + 1) != *(s1) ) 
