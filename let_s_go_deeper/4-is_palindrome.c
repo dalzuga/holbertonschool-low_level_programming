1308877c7b33fb578cd668b7e4f55058c9a55fd4
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-26 00:18:03 +0000

    task 4 done

--- let_s_go_deeper/4-is_palindrome.c
+++ let_s_go_deeper/4-is_palindrome.c
@@ -1,4 +1,29 @@
+int string_length(char * str);
+int helper (char *first, char *last, int i);
+
 int is_palindrome(char *s)
 {
+  int length = string_length(s);     /* stores length of string */
+  int leftofmiddle = length / 2 - 1; /* gets position of left of middle char  */
   
+  return helper(s, s + length - 1, leftofmiddle);
+}
+
+int string_length(char * str)
+{
+  if (*str == '\0')
+    return 0;
+
+  return 1 + string_length(str + 1);
+}
+
+int helper(char *first, char *last, int i)
+{
+  if (i == 0)
+    return 1;
+
+  if (*first != *last)
+    return 0;
+
+  return helper(first + 1, last - 1, i - 1);
 }
