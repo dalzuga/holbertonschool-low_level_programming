72664537bd0d036877b8977c9ebf403043b28496
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-26 19:15:36 +0000

    saving progress - added test

--- let_s_go_deeper/5-main.c
+++ let_s_go_deeper/5-main.c
@@ -6,15 +6,23 @@ int main(void)
 {
   int r;
   r = shell_comp("ab", "a*");
+  printf("ab, a*\n");
   printf("Result: %d\n", r);
   printf("Expected: 1\n");
   r = shell_comp("ab", "*b");
+  printf("ab, *b\n");
   printf("Result: %d\n", r);
   printf("Expected: 1\n");
+  printf("ab, *a\n");
   r = shell_comp("ab", "*a");
   printf("Result: %d\n", r);
   printf("Expected: 0\n");
   r = shell_comp("ab", "*c");
+  printf("ab, *c\n");
+  printf("Result: %d\n", r);
+  printf("Expected: 0\n");
+  r = shell_comp("xyza", "*a");
+  printf("xyza, *a\n");
   printf("Result: %d\n", r);
   printf("Expected: 0\n");
   printf("---------------------------\n");
