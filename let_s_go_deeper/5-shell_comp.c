ea86d805d6577c7fe5c5ccab56ea4108015b3204
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-26 19:12:38 +0000

    saving

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -17,8 +17,7 @@ int shell_comp(char *s1, char *s2)
     return shell_comp(s1 + 1, s2 + 1); /* move forward 1 character */
 
   /* "xyza" and "*a"; keep going until s2[1] matches s1[3] */
-  if (s2 == '*' && *(s2 + 1) != *(s1) )
+  if (*s2 == '*' && *(s2 + 1) != *(s1) )
     return shell_comp(s1 + 1, s2);
   return shell_comp(s1 + 2, s2 + 2);
-  
 }
