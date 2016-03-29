1cd378fd9f326d312c26fb9525f24c9a66e3af77
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-29 16:54:24 +0000

    task 1 done

--- neither_irony_or_sarcasm_is_argument/1-args.c
+++ neither_irony_or_sarcasm_is_argument/1-args.c
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
 {
   int i;
 
-  for (i = 0; i < argc - 1; i++)
+  for (i = 0; i < argc; i++) 	/* loop argc times */
   {
-    
+    printf("%s\n", argv[i]);	/* print */
   }
 
   return 0;
