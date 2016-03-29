0c978435886f87fe96d6d5f5c40df1cb15f4c86c
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-29 18:25:51 +0000

    saving progress - strings and chars are handled differently now

--- neither_irony_or_sarcasm_is_argument/2-options.c
+++ neither_irony_or_sarcasm_is_argument/2-options.c
@@ -4,6 +4,7 @@ int main(int argc, char **argv)
 {
   int i, j;
   char flag = 1;
+  char csflag = 0;		/* flag2 handles char vs string options */
 
   for (i = 1; i < argc; i++) 	/* loop argc times minus the one file name */
   {
@@ -12,10 +13,12 @@ int main(int argc, char **argv)
     if (argv[i][0] == '-')
     {      
       j++; 			/* move j forward */
+      csflag = 1;		/* case: char */
 
       if (argv[i][1] == '-')
       {
 	j++; 			/* move j forward */
+	csflag = 2;		/* case: string */
 
 	if (argv[i][2] == '\0') /* handle ignore '--' case */
 	  flag = 0;
@@ -25,6 +28,9 @@ int main(int argc, char **argv)
       {
 	printf("%c", argv[i][j]);
 	j++;
+
+	if (csflag == 1)
+	  printf(", ");
       }
       
       if (flag)
