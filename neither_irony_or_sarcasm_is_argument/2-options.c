43831948e2b30266be135dc727f55680e98bc94d
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-29 17:32:27 +0000

    saving progress

---
+++ neither_irony_or_sarcasm_is_argument/2-options.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  int i, j;
+
+  for (i = 1; i < argc; i++) 	/* loop argc times minus the one file name */
+  {
+    j = 0;
+
+    if (argv[i][0] == '-')
+    {      
+      j++; 			/* move j forward */
+
+      while (argv[i][j] != '\0')	/* print chars */
+      {
+	printf("%c", argv[i][j]);
+	j++;
+      }
+      
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
