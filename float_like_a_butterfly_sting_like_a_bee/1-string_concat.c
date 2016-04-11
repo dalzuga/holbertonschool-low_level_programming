0d7990160328f0e01db68229d25a34f27ec3e1e2
Daniel Alzugaray <dalzuga@gmail.com>
2016-04-11 10:32:33 +0000

    reverted and fixed

--- float_like_a_butterfly_sting_like_a_bee/1-string_concat.c
+++ float_like_a_butterfly_sting_like_a_bee/1-string_concat.c
@@ -25,17 +25,21 @@ char *string_concat(char *s1, char *s2)
 
   lengthtotal = length1 + length2 + 1; /* to accomodate null char */
 
-  strout = malloc(sizeof(char) * lengthtotal); /* allocate memory */
+  strout = malloc(sizeof(strout) * lengthtotal); /* allocate memory */
 
   for (i = 0; i < length1; i++) /* copy string 1 */
   {
-    strout[i] = s1[i];		/* copy char */
+    *strout = *s1; 		/* copy char */
+    s1++;			/* advance */
+    strout++;			/* advance */
   }
 
   for (i = 0; i <= length1; i++) /* copy string 2 + null char */
   {
-    strout[length1 + i] = s2[i];		/* copy char */
+    *strout = *s2;		/* copy char */
+    s2++;			/* advance */
+    strout++;			/* advance */
   }
 
-  return strout;
+  return strout - lengthtotal;
 }
