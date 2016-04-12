2cbfde6b48b46447fd4978579470094c5d87e9a0
Daniel Alzugaray <dalzuga@gmail.com>
2016-04-12 14:35:12 +0000

    count digits tested; works as expected

---
+++ float_like_a_butterfly_sting_like_a_bee/5-int_to_string.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+int count_digits(int);
+
+char *int_to_string(int n)
+{
+  char *str;
+  str = malloc(sizeof(str));
+
+  printf("Digits of %d are: %d\n", n, count_digits(n));
+
+  return str;
+}
+
+int count_digits(int n)
+{
+  int i, x;
+
+  i = 1; 			/* initialize i */
+  x = n;			/* copy n onto x */
+
+  if (n > 0)
+    x *= -1;
+
+  while (x < -9)		  /* while the number has more than 1 digit */
+  {
+    x = x / 10;
+    i++;
+  }
+
+  return i;
+}
