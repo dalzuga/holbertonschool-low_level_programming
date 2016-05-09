014b25c1ea5bac54d342ae159b3d9e3f19f37ec5
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-09 22:54:18 +0000

    code that at least compiles

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char __attribute__((unused)) *argv[]) /* get around compiler flag */
 {
-  if (argc < 2)
+  int fp;
+
+  if (argc < 2)			/* check usage */
     return 1;
   
-  printf("test\n");
+  fp = open("lorem_ipsum", O_RDONLY, S_IREAD);
+
+  if (fp == -1)
+    return 1;
+  
+  close(fp);
+
   return 0;
 }
