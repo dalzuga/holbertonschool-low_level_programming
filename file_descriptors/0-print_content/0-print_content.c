cc900449a345562891c1029118ef4e8217506e0b
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-09 23:15:46 +0000

    added print_char and using read

--- file_descriptors/0-print_content/0-print_content.c
+++ file_descriptors/0-print_content/0-print_content.c
@@ -3,18 +3,26 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int print_char(char c);
+
 int main(int argc, char __attribute__((unused)) *argv[]) /* get around compiler flag */
 {
   int fp;
+  char c;
 
   if (argc < 2)			/* check usage */
     return 1;
   
-  fp = open("lorem_ipsum", O_RDONLY, S_IREAD);
+  fp = open("lorem_ipsum", O_RDONLY);
 
   if (fp == -1)
     return 1;
   
+  while(read(fp, &c, 1) > 0)   /* read 1 byte and store it in char c */
+  {
+    print_char(c);
+  }
+
   close(fp);
 
   return 0;
