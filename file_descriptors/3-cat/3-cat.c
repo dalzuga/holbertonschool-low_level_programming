a1d549f7f2f60368780643706a3ef6a007de050b
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-10 22:29:09 +0000

    pushing task 3

---
+++ file_descriptors/3-cat/3-cat.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int print_char(char c);
+
+int main(int argc, char __attribute__((unused)) *argv[]) /* get around compiler flag */
+{
+  char c;
+
+  if (argc != 1)			/* check usage */
+  {
+    /* printf("usage\n"); */
+    return 1;
+  }
+
+  while(read(0, &c, 1) > 0)   /* read 1 byte and store it in char c */
+  {
+    if (c == 4)			/* if Ctrl-D EOT character (ASCII value 4) */
+      break;
+    print_char(c);
+  }
+
+  return 0;
+}
