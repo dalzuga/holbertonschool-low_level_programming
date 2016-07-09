8401a72a73130c5c90077072eb288e7944a3d8dd
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-09 23:32:26 +0000

    testing precedence

---
+++ d/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+        int i;
+
+        printf("Hi\n");
+
+        if ((i = 5) == 5)
+                printf("yes\n");
+        else
+                printf("no\n");
+        return 0;
+}
+
--- hash_tables/ht_create.c
+++ hash_tables/ht_create.c
@@ -1,6 +1,18 @@
 #include "hashtable.h"
 
+/* creates a hash table */
 HashTable *ht_create(unsigned int size)
 {
-        
+        HashTable *t;
+
+        if (size < 1)
+                return NULL;
+
+        if (t = malloc (sizeof(HashTable)) == NULL)
+                return NULL;
+
+        if (t->array = malloc(size * sizeof(List) == NULL)
+                return NULL;
+
+        return t;
 }
