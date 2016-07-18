4a6af5f2f19e6383ac368f73b89ba2212a87a8d4
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-18 17:29:36 +0000

    behavior now settled as expected

--- sorting_algorithms/quick_sort.c
+++ sorting_algorithms/quick_sort.c
@@ -14,26 +14,28 @@ void quick_sort(int *array, int size)
 void partition(int *array, int size)
 {
         int i, pivot, l, r;
-        l = -1;
+        l = r = -1;
 
         /* let's use the middle one, favoring the left if even */
         r = pivot = array[(size + 1) / 2 - 1];
         printf("PIVOT: %d\n", pivot);
         for (i = 0; i < (size + 1) / 2; i++) {
                 /* find element >= to pivot from left */
-                if (array[i] >= pivot && l == -1)
+                if (array[i] >= pivot)
                   {
                           l = i;
                           printf("Left\t: %d\n", array[l]);
+                          break;
                   }
         }
         
-        for (i = 0; i < (size + 1) / 2; i++) {
+        for (i = 0; i < size / 2 + 1; i++) {
                 /* find element <= to pivot from right */
                 if (array[size - i - 1] <= pivot)
                   {
                           r = size - i - 1;
                           printf("Right\t: %d\n", array[r]);
+                          break;
                   }
         }
 
@@ -48,9 +50,10 @@ void swap_and_continue(int *array, int l, int r, __attribute__ ((unused)) int si
         int tmp;
 
         if (l != r) {
+                printf("SWAP\n");
                 tmp = array[l];
                 array[l] = array[r];
                 array[r] = tmp;
-                partition(array,size);
+                /* partition(array, size); */
         }
 }
