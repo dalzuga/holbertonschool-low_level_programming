855db0562989275d70bc40a99af18bf961addb23
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-18 15:50:59 +0000

    second save [pre-test]

--- sorting_algorithms/quick_main.c
+++ sorting_algorithms/quick_main.c
@@ -4,20 +4,20 @@ void print_array(int *, int);
 int main(void)
 {
         int array[] = {402, 94111, 98, 12, 1024};
-        int array2[] = {7, 2, 8, 3, 5, 1, 9, 4, 6};
-        int array3[] = {7, 2, 8, 3, 5, 1, 9, 4, 6, 10};
+        int array2[] = {1, 2, 3, 4, 5, 6};
+        int array3[] = {6, 4, 2, 5, 3, 1};
 
         print_array(array, 5);
         quick_sort(array, 5);
         print_array(array, 5);
 
-        print_array(array2, 9);
-        quick_sort(array2, 9);
-        print_array(array2, 9);
+        print_array(array2, 6);
+        quick_sort(array2, 6);
+        print_array(array2, 6);
 
-        print_array(array3, 10);
-        quick_sort(array3, 10);
-        print_array(array3, 10);
+        print_array(array3, 6);
+        quick_sort(array3, 6);
+        print_array(array3, 6);
 
         return (0);
 }
--- sorting_algorithms/quick_sort.c
+++ sorting_algorithms/quick_sort.c
@@ -1,23 +1,44 @@
-void print_array(int *, int);
-int print_char(char);
-void print_number(int);
+#include <stdio.h>
+void partition(int *, int);
+void swap_and_continue(int *, int, int, int);
 
 /* sort an array using quick-sort algorithm */
 void quick_sort(int *array, int size)
 {
-        int i, pivot;
-
         if (size > 1) {
-                /* let's use the middle one */
-                pivot = array[(size + 1) / 2 - 1];
-                
-                print_number(pivot);
-                print_char('\n');
-
-                for (i = 0; i < (size + 1) / 2; i++) {
-                        print_number(i);
-                        print_char('\t');
-                }
-                print_char('\n');
+                partition(array, size);
         }
+}
+
+/* partition the elements */
+void partition(int *array, int size)
+{
+        int i, pivot, l, r;
+        l = -1;
+
+         /* let's use the middle one, favoring the left if even */
+        r = pivot = array[(size + 1) / 2 - 1];
+
+        for (i = 0; i < (size + 1) / 2; i++) {
+                /* find element >= to pivot from left */
+                if (array[i] >= pivot && l == -1)
+                        l = i;
+        }
+        
+        for (i = 0; i < (size + 1) / 2; i++) {
+                /* find element < to pivot from right */
+                if (array[size - i] < pivot)
+                        r = size - i - 1;
+        }
+
+        if (l != -1)
+                swap_and_continue(array, l, r, size);
+}
+
+/* swap two array elements and continue the partition */
+void swap_and_continue(int *array, int l, int r, __attribute__ ((unused)) int size) {
+        int tmp;
+        tmp = array[l];
+        array[l] = array[r];
+        array[r] = tmp;
 }
