ad379997217a393a357c5357d05f399ea21a1672
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-18 14:45:27 +0000

    first save

--- sorting_algorithms/quick_sort.c
+++ sorting_algorithms/quick_sort.c
@@ -1,4 +1,20 @@
+void print_array(int *, int);
+int print_char(char);
+void print_number(int);
+
+/* sort an array using quick-sort algorithm */
 void quick_sort(int *array, int size)
 {
-        
+        int i, pivot;
+
+        if (size > 1) {
+                pivot = array[(size + 1) / 2];
+                
+                print_number(pivot);
+                print_char('\n');
+
+                for (i = 0; i < size / 2; i++) {
+                        print_number(i);
+                }
+        }
 }
