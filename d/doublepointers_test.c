4fec01700a3c9e9a57e90c0d5402e157144d6891
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-20 19:05:04 +0000

    save

---
+++ d/doublepointers_test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int **dp;
+
+        dp = malloc(sizeof(void *));
+        *dp = malloc(sizeof(void *));
+        /* **dp = malloc(sizeof(int)); */
+        **dp = 10;
+
+        printf("double pointer address\nhex:\t%p\n", &dp);
+        printf("-----------------------\n");
+        printf("intermediate pointer address\nhex:\t%p\n", dp);
+        printf("-----------------------\n");
+        printf("address of the int\nhex:\t%p\n", *dp);
+        printf("-----------------------\n");
+        printf("value of int\n:\t%d\n", **dp);
+}
+
