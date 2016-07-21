75ab509445625714c3dd91ade8411fdb047051ce
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-21 15:17:58 +0000

    added change for more clarity

--- d/test_double_pointer.c
+++ d/test_double_pointer.c
@@ -7,16 +7,18 @@ int main()
 
         dp = malloc(sizeof(void *));
         *dp = malloc(sizeof(void *));
+        /* the below malloc is done automatically, not correct */
         /* **dp = malloc(sizeof(int)); */
         **dp = 10;
 
         printf("double pointer address\nhex:\t%p\n", (void *) &dp);
+        printf("double pointer value\nhex:\t%p\n", (void *) dp);
         printf("-----------------------\n");
-        printf("intermediate pointer address\nhex:\t%p\n", (void *) dp);
+        printf("intermediate pointer address\nhex:\t%p\n", (void *) &*dp);
+        printf("intermediate pointer value\nhex:\t%p\n", (void *) *dp);
         printf("-----------------------\n");
-        printf("address of the int\nhex:\t%p\n", (void *) *dp);
-        printf("-----------------------\n");
-        printf("value of int\nint:\t%d\n", **dp);
+        printf("address of the int\nhex:\t%p\n", (void *) &**dp);
+        printf("value of the int\nint:\t%d\n", **dp);
 
         return 0;
 }
