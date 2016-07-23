c76ac30a6555176493fec599a584bbce179877a0
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-23 21:09:18 +0000

    mixed [bac] recursion working

--- trees/main.c
+++ trees/main.c
@@ -14,18 +14,18 @@ int main()
 
         tree = NULL;
 
-	printf("insert:\tc\n");
-        if (btree_insert(&tree, "c")) /* error check */
-                return 1;
-	printf("------------------\n");
-
 	printf("insert:\tb\n");
-	if (btree_insert(&tree, "b")) /* error check */
-		return 1;
+        if (btree_insert(&tree, "b")) /* error check */
+                return 1;
 	printf("------------------\n");
 
 	printf("insert:\ta\n");
 	if (btree_insert(&tree, "a")) /* error check */
+		return 1;
+	printf("------------------\n");
+
+	printf("insert:\tc\n");
+	if (btree_insert(&tree, "c")) /* error check */
 		return 1;
 	/* printf("-------PRINTING STARTS NOW-----------\n"); */
 
