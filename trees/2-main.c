0f2180d5537638ecf1e3bc97da74b7a01ec77592
Daniel Alzugaray <dalzuga@gmail.com>
2016-07-24 20:24:49 +0000

    init task 2

---
+++ trees/2-main.c
@@ -0,0 +1,47 @@
+#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int btree_insert(BTree **tree, char *data);
+void print_preorder(BTree *tree);
+BTree *btree_find(BTree *tree, char *str);
+
+int main()
+{
+        BTree *tree;
+
+        tree = NULL;
+
+	printf("-------DANIEL'S TREE STARTS NOW-----------\n");
+
+	printf("insert:\tb\n");
+        if (btree_insert(&tree, "b")) /* error check */
+                return 1;
+
+	printf("insert:\ta\n");
+	if (btree_insert(&tree, "a")) /* error check */
+		return 1;
+
+	printf("insert:\tb\n");
+        if (btree_insert(&tree, "b")) /* error check */
+                return 1;
+
+	print_preorder(tree);
+
+	if (btree_find(tree, "b") == NULL)
+		printf("Not found.");
+	else
+		printf("Found!");
+
+        return 0;
+}
+
+void print_preorder(BTree *tree)
+{
+	if (tree == NULL)
+		return;
+
+	printf("%s\n", tree->str);
+	print_preorder(tree->left);
+	print_preorder(tree->right);
+}
