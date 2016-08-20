3617959be7233118e3a51018ba29ab775ffc0e42
Daniel Alzugaray <dalzuga@gmail.com>
2016-08-20 16:43:13 +0000

    save

--- hackerrank/implementation/non-divisible-subset.c
+++ hackerrank/implementation/non-divisible-subset.c
@@ -9,10 +9,11 @@ int count_positive_elements(int *arr, int size);
 int main() {
 
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-	int *arr;
-	int n, k, i, j;
+	int n, k, i, j, count_pairs, count_freq;
+	scanf("%d %d", &n, &k);
+	int arr[n];
 
-	scanf("%d", n);
+	count_pairs = 0;
 
 	for (i = 0; i < n; i++)
 	{
@@ -21,10 +22,35 @@ int main() {
 	
 	for (i = 0; i < n; i++)
 	{
+		for (j = i; j < n; j++)
+		{
+			if (i == j)
+			{}
+			else if ((arr[i] + arr[j]) % k == 0)
+			{
+				printf("(%d, %d)\n", arr[i], arr[j]);
+				count_pairs++;
+			}
+		}
+	}
+
+	printf("count_pairs: %d\n", count_pairs);
+
+	count_freq = 0;
+
+	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < n; j++)
 		{
-			
+			if (i == j)
+			{}
+			else if ((arr[i] + arr[j]) % k == 0)
+			{
+				count_freq++;
+			}
 		}
+		printf("%d appears %d times.\n", arr[i], count_freq);
+		count_freq = 0;
 	}
 
 	return (0);
@@ -32,10 +58,7 @@ int main() {
 
 int maximal_sprime(int *arr, int size)
 {
-	int i, j, k;
 	int a;
-
-	
 
 	a = count_positive_elements(arr, size);
 	return a;
