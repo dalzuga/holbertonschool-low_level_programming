815f9692d723761f46dadf7a42bf9eec5897138b
Daniel Alzugaray <dalzuga@gmail.com>
2016-08-22 21:14:47 +0000

    starting task-scheduling

--- hackerrank/implementation/non-divisible-subset.c
+++ hackerrank/implementation/non-divisible-subset.c
@@ -9,34 +9,22 @@ int count_positive_elements(int *arr, int size);
 int main() {
 
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-	int n, k, i, j, count_pairs, count_freq;
+	int n, k, i;
 	scanf("%d %d", &n, &k);
 	int arr[n];
-
-	count_pairs = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &(arr[i]));
 	}
 	
-	int arr2[n];
-	memset(arr2, 0, n * sizeof(int));
-
-	printf("%d", arr[0] % k);
-	arr2[arr[0] % k]++;
-        for (i = 1; i < n; i++)
+        for (i = 0; i < n; i++)
 	{
-		printf(" %d", arr[i] % k);
-		arr2[arr[i] % k]++;
+		if (i != 0)
+			printf(" ");
+		printf("%d", arr[i] % k);
 	}
 	printf("\n");
-
-	for (i = 0; i < n; i++)
-	{
-		if (arr2[i] != 0)
-			printf("arr2[%d] = %d\n", i, arr2[i]);
-	}
 
 	return (0);
 }
