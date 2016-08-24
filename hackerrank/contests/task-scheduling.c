ade17d1ecb641f7cddd77bc942cb138922ce4ac6
Daniel Alzugaray <dalzuga@gmail.com>
2016-08-24 01:09:58 +0000

    add backup file

--- hackerrank/contests/task-scheduling.c
+++ hackerrank/contests/task-scheduling.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #define ABS(x) ((x)<0 ? -(x) : (x))
+#define LINE 0
 
 int main() {
 	int n;
@@ -13,13 +14,14 @@ int main() {
 	int i;
 	int sum_prev, sum_curr;
 	int maxmin_prev, maxmin_curr;
+	char flag;
 
 	for (i = 0; i < n; i++)	/* input */
 	{
 		scanf("%d %d", &d[i], &m[i]);
 	}
 
-	sum_prev = sum_curr = maxmin_prev = maxmin_curr = 0;
+	flag = sum_prev = sum_curr = maxmin_prev = maxmin_curr = 0;
 
 	for (i = 0; i < n; i++)
 	{
@@ -28,21 +30,32 @@ int main() {
 			maxmin_curr = d[i];
 		if (sum_curr < maxmin_curr)
 		{
-			/* printf("trippin the flag\n"); */
-			sum_curr = sum_prev;
-			maxmin_curr = maxmin_prev;
+			printf("TRIP\n");
+			flag = 1;
 		}
 
-		/* if (34 < i && i < 50) */
-		/* { */
-			/* printf("--------------line %d----------\n", i); */
-			/* printf("sum_curr:\t%d\n", sum_curr); */
-			/* printf("m[%d]:\t%d\n", i, m[i]); */
+		/* printf("--------------line %d----------\n", i); */
+		/* printf("sum_curr:\t%d\n", sum_curr); */
+		/* printf("m[%d]:\t%d\n", i, m[i]); */
+		printf("\t");
+		if (flag == 0)	/* never tripped */
+		{
 			printf("%d\n", ABS(sum_curr - maxmin_curr));
-		/* } */
-
-		maxmin_prev = maxmin_curr;
-		sum_prev = sum_curr;
+			maxmin_prev = maxmin_curr;
+			sum_prev = sum_curr;
+		}
+		else if (flag == 1) /* tripped once */
+		{
+			printf("%d\n", ABS(sum_prev - maxmin_prev));
+			printf("UNTRIP\n");
+			flag = 2;
+		}
+		else if (flag == 2) /* tripped more than once */
+		{
+			printf("%d\n", ABS(sum_curr - maxmin_curr));
+			maxmin_prev = maxmin_curr;
+			sum_prev = sum_curr;
+		}
 	}
 
 	return 0;
