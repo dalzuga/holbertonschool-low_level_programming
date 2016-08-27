ddc8339d10d06e4489ea20ad8db411a7aa64cb1c
Daniel Alzugaray <dalzuga@gmail.com>
2016-08-27 17:34:32 +0000

    saving progress

--- hackerrank/contests/task-scheduling.c
+++ hackerrank/contests/task-scheduling.c
@@ -14,40 +14,28 @@ int main() {
 	int i;
 	int sum_prev, sum_curr;
 	int maxmin_prev, maxmin_curr;
-	char flag;
 
 	for (i = 0; i < n; i++)	/* input */
 	{
 		scanf("%d %d", &d[i], &m[i]);
 	}
 
-	flag = sum_prev = sum_curr = maxmin_prev = maxmin_curr = 0;
+	sum_prev = sum_curr = maxmin_prev = maxmin_curr = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		sum_curr += m[i];
 		if (maxmin_curr < d[i]) /* get the biggest deadline */
 			maxmin_curr = d[i];
-		if (sum_curr < maxmin_curr)
+		if (sum_curr < maxmin_curr) /* if the result goes negative */
 		{
-			printf("TRIP\n");
-			flag = 1;
+			printf("%d\n", ABS(sum_prev - maxmin_prev)); /* print prev result */
 		}
-
-		/* printf("--------------line %d----------\n", i); */
-		/* printf("sum_curr:\t%d\n", sum_curr); */
-		/* printf("m[%d]:\t%d\n", i, m[i]); */
-		printf("\t");
-		if (flag == 0)	/* never tripped */
+		else
 		{
 			printf("%d\n", ABS(sum_curr - maxmin_curr)); /* print the real sum */
 			maxmin_prev = maxmin_curr; /* keep updating prev */
 			sum_prev = sum_curr;
-		}
-		else	/* flag is 1 */
-		{
-			printf("%d\n", ABS(sum_prev - maxmin_prev)); /* print the previously stored data */
-			flag = 0;
 		}
 	}
 
