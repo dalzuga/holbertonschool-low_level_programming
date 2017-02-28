40465dafe6ef20e18661c65810c17087fffb4cc3
Daniel Alzugaray <dalzuga@gmail.com>
2017-02-27 18:19:41 -0800

    Trying hash table.

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size) {
+	hash_table_t *ht_ptr;
+	ht_ptr = malloc(sizeof(hash_table_t));
+	ht_ptr->size = 0;
+	ht_ptr->array = NULL;
+
+	ht_ptr->array=malloc(sizeof(hash_node_t));
+	if (ht_ptr->array == NULL) {
+		return NULL;
+	}
+	ht_ptr->size = size;
+
+	return ht_ptr;
+}
