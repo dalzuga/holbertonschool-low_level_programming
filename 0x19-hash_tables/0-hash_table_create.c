578425457a7b07bd5f423c506143ec44ceb21dc1
Daniel Alzugaray <dalzuga@gmail.com>
2017-02-27 19:22:45 -0800

    Fixing issue. Tested with valgrind.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -3,12 +3,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size) {
 	hash_table_t *ht_ptr;
+
+	if (size < 1)
+	{
+		return NULL;
+	}
+
 	ht_ptr = malloc(sizeof(hash_table_t));
 	ht_ptr->size = 0;
 	ht_ptr->array = NULL;
 
-	ht_ptr->array=malloc(sizeof(hash_node_t) * size);
-	if (ht_ptr->array == NULL) {
+	ht_ptr->array=malloc(sizeof(char *) * size);
+	if (ht_ptr->array == NULL)
+	{
 		return NULL;
 	}
 	ht_ptr->size = size;
