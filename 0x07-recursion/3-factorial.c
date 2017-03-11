f4ba6e4b35891442a21fb19abd9d45b148c1b68a
Daniel Alzugaray <dalzuga@gmail.com>
2017-03-10 19:39:58 -0800

    Pushing task 3.

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * factorial - recursive factorial calculation
+ *
+ * @n: number to apply the factorial to
+ * 
+ * Return: the factorial of n.
+ */
+int factorial(int n)
+{
+	/* factorial of 0 is 1 */
+	if (n == 0)
+	{
+		return 1;
+	}
+
+	if (n < 0)
+	{
+		return -1;
+	}
+
+	return n * factorial(n - 1);
+}
