ada14e117912672d0636b106825edc84dafb7211
Daniel Alzugaray <dalzuga@gmail.com>
2017-03-10 18:57:28 -0800

    Pushing task 1.

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: Pointer to a string.
+ *
+ * Return: No return.
+ */
+void _print_rev_recursion(char *s)
+{
+	/* if not NULL char */
+	if (*s == '\0')
+	{
+		return;
+	}
+	
+	_print_rev_recursion(s+1);
+
+	_putchar(*s);
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -5,4 +5,6 @@
 int _putchar(char c);
 /* my recursive print function */
 void _puts_recursion(char *s);
+/* my reverse recursive print function */
+void _print_rev_recursion(char *s);
 #endif
