f8438a84b1733bbaeb4f5bfc592ef558228e52da
Daniel Alzugaray <dalzuga@gmail.com>
2017-03-10 16:14:45 -0800

    Pushing syle changes. -  Adding comment and improving readability.
    -  Removing space for Betty Holberton check.

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -6,10 +6,10 @@
  *
  * Return: No return.
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	/* if not NULL char */
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,2 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* _putchar - prints a single character */
 int _putchar(char c);
+/* my recursive print function */
 void _puts_recursion(char *s);
+#endif
