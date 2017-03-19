1f6e911e8c8d30e9450ac8e6e012e30e140212ac
Daniel Alzugaray <dalzuga@gmail.com>
2017-03-18 21:18:55 -0700

    Pushing 0x12 - task 0.

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_listint - prints a simple linked list.
+ *
+ * @h - pointer to first simple linked list iteml
+ *
+ * Return: how many items in the simple linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	/* Variable declarations */
+	int ctr;
+
+	/* Initializations */
+	ctr = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		ctr++;
+	}
+
+	return (ctr);
+}
