aa95d0d48f8e13334161b88ad2f9e3de6e1d8593
Daniel Alzugaray <dalzuga@gmail.com>
2017-03-20 16:02:47 -0700

    Beating the style checker.

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -13,24 +13,20 @@ void free_listint2(listint_t **head)
 	/* Variable declarations */
 	listint_t *tmp_ptr, *trl_ptr;
 
-	/* Checking for valid addresses */
 	if (head == NULL)
 	{
 		return;
 	}
 
-	/* Initializations */
 	tmp_ptr = *head;
 	trl_ptr = NULL;
 
-	/* ll is empty */
 	if (*head == NULL)
 	{
 		head = NULL;
 		return;
 	}
 
-	/* ll has one item */
 	if ((*head)->next == NULL)
 	{
 		free(*head);
@@ -39,20 +35,16 @@ void free_listint2(listint_t **head)
 		return;
 	}
 
-	/* Traverse ll until it points to last node */
 	while (tmp_ptr->next != NULL)
 	{
 		trl_ptr = tmp_ptr;
 		tmp_ptr = tmp_ptr->next;
 	}
 
-	/* Delete reference to next in second to last node */
 	trl_ptr->next = NULL;
 
-	/* Free the last node */
 	free(tmp_ptr);
 
-	/* Re-initialize tmp_ptr */
 	tmp_ptr = NULL;
 
 	free_listint2(head);
