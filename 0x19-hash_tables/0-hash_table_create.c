c8c09e92dbcdbe0148b10946a1dcc74926f42d52
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-10 15:11:57 -0700

    Updating Betty style/doc.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,28 +2,35 @@
 #include "hash_tables.h"
 #include <stdio.h>
 
-hash_table_t *hash_table_create(unsigned long int size) {
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of table.
+ *
+ * Return: pointer to hash table created, NULL on error.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
 	hash_table_t *ht_ptr;
-	long unsigned int i;
+	unsigned long int i;
 
 	/* check for valid size */
 	if (size < 1)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	ht_ptr = malloc(sizeof(hash_table_t));
 	if (ht_ptr == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	ht_ptr->size = 0;
 	ht_ptr->array = NULL;
 
-	ht_ptr->array=malloc(sizeof(hash_node_t *) * size);
+	ht_ptr->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht_ptr->array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	ht_ptr->size = size;
 
@@ -37,5 +44,5 @@ hash_table_t *hash_table_create(unsigned long int size) {
 	/* pointer arithmetic to return ~ht_ptr->array~ to the beginning */
 	ht_ptr->array = (ht_ptr->array) - i;
 
-	return ht_ptr;
+	return (ht_ptr);
 }
