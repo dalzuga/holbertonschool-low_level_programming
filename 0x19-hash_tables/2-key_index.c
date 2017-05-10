4950ec68c82d2f2b617e34a6e0efdc6f0886d57b
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-10 15:22:01 -0700

    Updating Betty style/doc.

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,11 +1,19 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - returns index depending on table size
+ * @key: the key string
+ * @size: the hash table size
+ *
+ * Return: The index key generated.
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
+
 	index = 0;
 
-	index = hash_djb2(key) % size;
+	index = hash_djb2(key) % size; /* set key index according to size */
 
 	return (index);
 }
