bf213d03d705ae7f9ab56874cf7593edf70197e5
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-10 13:36:29 -0700

    Updating Betty style.

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - recursive factorial calculation
  *
  * @x: number to apply the exponent to
- * @y: exponent 
+ * @y: exponent
  *
  * Return: the result of x^y
  */
@@ -12,14 +12,14 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
 
 	/* case y = 0 */
 	if (y == 0)
 	{
-		return 1;
+		return (1);
 	}
 
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
