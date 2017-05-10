cbd55fd98b7667e4230a08b57f020f31762cf4d3
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-10 15:14:15 -0700

    Updating Betty style/doc.

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,14 +1,20 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - returns a hash based on djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: the hash result.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long hash = 5381;
-        int c;
+	unsigned long hash = 5381;
+	int c;
 
-        while ((c = *str++))
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 
-        return (hash);
+	return (hash);
 }
