0e823f55712e01c0c459f2ec1e0dfdb2552d8a12
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-14 16:16:17 -0700

    Saving progress.

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -100,6 +100,39 @@ hash_node_t *make_node(const char *key, const char *value)
 }
 
 /**
+ * invalid_ht - checks if the hash table is considered invalid
+ *
+ * @ht: address of hash table to check.
+ *
+ * Return: 0 if valid, 1 otherwise.
+ */
+int invalid_ht(hash_table_t *ht)
+{
+	if (ht == NULL)
+	{
+		_printf("hash_table_delete.c: ");
+		_printf("---1---\n");
+		return (1);
+	}
+
+	if (ht->size < 1)
+	{
+		_printf("hash_table_delete.c: ");
+		_printf("---2---\n");
+		return (1);
+	}
+
+	if (ht->array == NULL)
+	{
+		_printf("hash_table_delete.c: ");
+		_printf("---3---\n");
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
  * _strlen - gets the length of a string.
  *
  * @s: input string.
@@ -149,4 +182,16 @@ char *_strdup(const char *s1)
 	}
 
 	return (s2);
+}
+
+/**
+ *
+ *
+ */
+void _printf(const char *s)
+{
+	if (DEBUG)
+	{
+		printf("%s", s);
+	}
 }
