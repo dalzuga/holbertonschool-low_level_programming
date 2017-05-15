be8d298e70691462f85db22e42e01243d1f0deee
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-15 05:09:55 -0700

    Modified to match spec.

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -16,6 +16,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp_ptr;
+	char flag = 0;
 
 	/* Inits */
 	i = 0;
@@ -31,6 +32,8 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 	}
 
+	printf("{");
+
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp_ptr = *(ht->array + i);
@@ -39,12 +42,18 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (tmp_ptr->key == NULL)
 			{
-				/* printf("Error: invalid hash table."); */
+				return;
 			}
-			printf("[%lu]: ", i);
+			if (flag == 1)
+			{
+				printf(", ");
+			}
 			print_linked_list(tmp_ptr);
+			flag = 1;
 		}
 	}
+
+	printf("}\n");
 }
 
 /**
@@ -64,9 +73,7 @@ void print_linked_list(hash_node_t *first_node_ptr)
 
 	while (tmp_ptr != NULL)
 	{
-		printf("{ %s : %s } --> ", tmp_ptr->key, tmp_ptr->value);
+		printf("'%s': '%s'", tmp_ptr->key, tmp_ptr->value);
 		tmp_ptr = tmp_ptr->next;
 	}
-
-	printf("NULL\n");
 }
