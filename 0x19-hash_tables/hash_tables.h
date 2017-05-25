a533b063172c23dc6fcfb8b7a03082aafc0e8777
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-25 08:12:12 -0700

    Starting task 100.

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -30,6 +30,43 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ * @sprev: A pointer to the previous element of the sorted linked list
+ * @snext: A pointer to the next element of the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ * @shead: A pointer to the first element of the sorted linked list
+ * @stail: A pointer to the last element of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
 #ifndef DEBUG
 #define DEBUG 0
 #endif
@@ -90,5 +127,29 @@ int ht_set_helper(hash_node_t **head, const char *key, const char *value);
 
 /* ht_node_update - updates the value in a node */
 int ht_node_update(hash_node_t *tmp_node, const char *value);
+
+/* shash_table_create - creates a sorted hash table */
+shash_table_t *shash_table_create(unsigned long int size);
+
+/* shash_table_set - set a key-value pair in a sorted hash table */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+
+/* shash_table_get - gets a value from the key in a sorted hash table */
+char *shash_table_get(const shash_table_t *ht, const char *key);
+
+/*
+ * shash_table_print - prints a sorted hash table. If the hash table is not
+ * valid, it will not print anything.
+ */
+void shash_table_print(const shash_table_t *ht);
+
+/*
+ * hash_table_print - prints a sorted hash table in reverse order. If the hash
+ * table is not valid, it will not print anything.
+ */
+void shash_table_print_rev(const shash_table_t *ht);
+
+/* shash_table_delete - frees all memory allocated to a sorted hash table. */
+void shash_table_delete(shash_table_t *ht);
 
 #endif
