403c041e4a8f6b48684891d1e296433716796828
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-25 04:17:38 -0700

    Add hash table collision to main file.

--- 0x19-hash_tables/5-main.c
+++ 0x19-hash_tables/5-main.c
@@ -13,6 +13,14 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
+    /* hash_table_set(ht, "c", "fun"); */
+    /* hash_table_set(ht, "python", "awesome"); */
+    /* hash_table_set(ht, "Jennie", "and Jay love asm"); */
+    /* hash_table_set(ht, "N", "queens"); */
+    /* hash_table_set(ht, "Asterix", "Obelix"); */
+    /* hash_table_set(ht, "Betty", "Holberton"); */
+    /* hash_table_set(ht, "98", "Battery Street"); */
+
     hash_table_set(ht, "c", "fun");
     hash_table_set(ht, "python", "awesome");
     hash_table_set(ht, "Jennie", "and Jay love asm");
@@ -20,6 +28,10 @@ int main(void)
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Holberton");
     hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "hetairas", "holberton");
+    hash_table_set(ht, "mentioner", "hello");
+
+
     hash_table_print(ht);
     hash_table_delete(ht);
     return (EXIT_SUCCESS);
