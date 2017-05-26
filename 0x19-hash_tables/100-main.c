6e0e8199cb2240688488b8dc80b28829f73c383c
Daniel Alzugaray <dalzuga@gmail.com>
2017-05-26 09:47:39 -0700

    ccccc - just realized task wants them alphabetically

--- 0x19-hash_tables/100-main.c
+++ 0x19-hash_tables/100-main.c
@@ -18,18 +18,18 @@ int main(void)
 	shash_table_print(ht);
 	shash_table_set(ht, "j", "1");
 	shash_table_print(ht);
-	/* shash_table_set(ht, "c", "2"); */
-	/* shash_table_print(ht); */
-	/* shash_table_set(ht, "b", "3"); */
-	/* shash_table_print(ht); */
-	/* shash_table_set(ht, "z", "4"); */
-	/* shash_table_print(ht); */
-	/* shash_table_set(ht, "n", "5"); */
-	/* shash_table_print(ht); */
-	/* shash_table_set(ht, "a", "6"); */
-	/* shash_table_print(ht); */
-	/* shash_table_set(ht, "m", "7"); */
-	/* shash_table_print(ht); */
+	shash_table_set(ht, "c", "2");
+	shash_table_print(ht);
+	shash_table_set(ht, "b", "3");
+	shash_table_print(ht);
+	shash_table_set(ht, "z", "4");
+	shash_table_print(ht);
+	shash_table_set(ht, "n", "5");
+	shash_table_print(ht);
+	shash_table_set(ht, "a", "6");
+	shash_table_print(ht);
+	shash_table_set(ht, "m", "7");
+	shash_table_print(ht);
 	/* shash_table_print_rev(ht); */
         /* shash_table_delete(ht); */
 	return (EXIT_SUCCESS);
